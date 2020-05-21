43bc7f18b26da0afe01aab7a399e824c01b05a17
BM Alcanar <birgermartin.alcanar@98labs.com>
2020-05-21 23:31:03 +0800

    Add basic home page lazy routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bulsuan-project-web';
-}
+export class AppComponent {}
